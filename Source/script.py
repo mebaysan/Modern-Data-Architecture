2e5f66491c31a612e33dc8627a22a9caf6575932
mebaysan <menesbaysan@gmail.com>
2022-04-08 16:40:46 +0300

    loader script schema bug fix

--- Source/script.py
+++ Source/script.py
@@ -9,11 +9,15 @@ load_dotenv()
 
 engine = create_engine(f"postgresql://{os.getenv('DB_STRING')}")
 
+engine.execute("""
+                CREATE SCHEMA IF NOT EXISTS ecommerce
+                """)
+
 filenames = glob.glob('*.csv')
 for filename in filenames:
     dataset_name = filename.split('_dataset')[0]
     dataset = pd.read_csv(filename)
     dataset.to_sql(
-        name=dataset_name, con=engine, schema="public", if_exists="replace"
+        name=dataset_name, con=engine, schema="ecommerce", if_exists="replace"
     )
     print(f'SUCCEED: {filename} -> {dataset_name}')
